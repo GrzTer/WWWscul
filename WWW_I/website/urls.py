b0261fbd7cbe7e9507d98b8ccecfea0e1547b8a9
GrzegorzT <grzegorz.tereszkiewicz@infotech.edu.pl>
2024-11-27 09:54:29 +0100

    ....

--- WWW_I/website/urls.py
+++ WWW_I/website/urls.py
@@ -5,5 +5,33 @@ urlpatterns = [
     path("<int:id>", views.detail, name="detail"),
 
     path("room", views.room_list, name="room"),
+    path("new", views.new, name="new"),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # // Copyright (c) 2024 Grzegorz Tereszkiewicz all rights reserved.
--- WWW_I/website/views.py
+++ WWW_I/website/views.py
@@ -1,11 +1,10 @@
-
+from django.forms import modelform_factory
 from django.http import HttpResponse
 from datetime import datetime
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
 from website.models import Meeting, Room
 
-
+MeetingForm=modelform_factory(Meeting,exclude=[])
 def welcome(request):
     num_meetings = Meeting.objects.count()
     meeting = Meeting.objects.all()
@@ -37,4 +36,53 @@ def me(request):
 def room_list(request):
     room = Room.objects.all()
     return render(request, "website/room_list.html", {"room": room})
+
+def new(request):
+    form=MeetingForm()
+    if request.method=="POST":
+        form=MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("website:welcome")
+    else:
+        form=MeetingForm()
+    return render(request, "website/new.html", {"form":form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # // Copyright (c) 2024 Grzegorz Tereszkiewicz all rights reserved.

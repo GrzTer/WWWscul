8c02537cee8e569b0bff6afeee8775f856dbcccd
GrzegorzT <grzegorz.tereszkiewicz@infotech.edu.pl>
2025-03-04 09:28:56 +0100

    Oglapp2

--- ogloszenie/ogloszenie/o_app/urls.py
+++ ogloszenie/ogloszenie/o_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, tresc, ksiazki
 
 urlpatterns = [
     path('', index, name='index'),
+    path('ogloszenie/<int:pk>/', tresc, name='tresc'),
+    path('ksiazki', ksiazki, name='ksiazki'),
 ]
--- ogloszenie/ogloszenie/o_app/views.py
+++ ogloszenie/ogloszenie/o_app/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Ogloszenia
 
 def index(request): 
     ogloszenia = Ogloszenia.objects.all()
     ogloszenia_l = Ogloszenia.objects.count()
     return render(request, 'index.html', {'ogloszenia': ogloszenia, 'ogloszenia_l': ogloszenia_l})
+
+def tresc(request, pk):
+    tresc = get_object_or_404(Ogloszenia, pk=pk)
+    return render(request, 'tresc.html', {'tresc': tresc})
+
+def ksiazki(request):
+    ksiazka = Ogloszenia.objects.filter(kategoria=1)
+    return render(request, 'ksiazki.html', {'ksiazka': ksiazka})
